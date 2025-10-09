8be8377e5e49e5a857b724634ccca983f7c1f20c
QuangDepChai <quanglm@epoints.vn>
2025-10-09 17:21:54 +0700

    update version

--- lib/chat.dart
+++ lib/chat.dart
@@ -15,7 +15,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/material.dart';
 
 import 'data_model/room.dart' as r;
-
+//____________2.1.5
 class Chat {
   static const MethodChannel _channel = MethodChannel('chat');
   static Future<String?> get platformVersion async {
